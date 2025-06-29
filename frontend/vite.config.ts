39ddfeac6931670f0a2bac428d0ab76e0d451407
ishanmehra <ishanmehra04@gmail.com>
2025-06-29 17:09:41 +0530

    minor changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5174,
     proxy: {
-      '/books': 'http://localhost:3000',
+      '/books': 'http://localhost:3001',
     },
   },
 }); 
